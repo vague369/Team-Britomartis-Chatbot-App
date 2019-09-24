d9e938796ddb84c76167ae6e4cde40c8893c2aa8
Vague <vaguemail369@gmail.com>
2019-09-24 22:18:07 +0100

    Create the app Repository

---
+++ app/src/main/java/com/britomartis/android/britobudget/data/MessageRepository.kt
@@ -0,0 +1,21 @@
+package com.britomartis.android.britobudget.data
+
+class MessageRepository private constructor(private val messageDao: MessageDao) {
+
+    fun getMessages() = messageDao.getMessages()
+    fun insertMessage(message: Message) {
+        messageDao.insertMessage(message)
+    }
+
+    companion object {
+
+        // For Singleton instantiation
+        @Volatile
+        private var instance: MessageRepository? = null
+
+        fun getInstance(messageDao: MessageDao) =
+            instance ?: synchronized(this) {
+                instance ?: MessageRepository(messageDao).also { instance = it }
+            }
+    }
+}

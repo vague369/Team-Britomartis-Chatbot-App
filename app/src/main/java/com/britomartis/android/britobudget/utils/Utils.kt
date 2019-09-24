380d69105050436c414c96c2a8f8b8a7e0ff417d
Vague <vaguemail369@gmail.com>
2019-09-25 00:27:15 +0100

    Return "Yesterday" if the date was the previous day

--- app/src/main/java/com/britomartis/android/britobudget/utils/Utils.kt
+++ app/src/main/java/com/britomartis/android/britobudget/utils/Utils.kt
@@ -16,5 +16,16 @@ fun convertTimeLongToString(time: Long): String {
 }
 
 fun convertTimeLongToDateString(time: Long): String {
+    // Check if the time was yesterday
+    val c1 = Calendar.getInstance()
+    c1.add(Calendar.DAY_OF_YEAR, -1)
+    val c2 = Calendar.getInstance()
+    c2.timeInMillis = time
+
+    if (c1.get(Calendar.YEAR) == c2.get(Calendar.YEAR) && c1.get(Calendar.DAY_OF_YEAR) == c2.get(Calendar.DAY_OF_YEAR)) {
+        return "Yesterday"
+    }
+
+    // Else just return the formatted date
     return SimpleDateFormat("MMMM d, YYYY", Locale.getDefault()).format(time)
 }
